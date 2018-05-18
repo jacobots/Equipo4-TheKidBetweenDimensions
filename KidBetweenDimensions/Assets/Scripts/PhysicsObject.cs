1b4e67a4b5f92affe317700468eab07d01d3cb3d
Juan Felipe Gomez <juanfgomez0912@gmail.com>
2018-05-18 02:09:00 -0500

    Inicio programación controles del personaje

---
+++ KidBetweenDimensions/Assets/Scripts/PhysicsObject.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicsObject : MonoBehaviour {
+
+    public float gravityModifier = 1f;
+    protected Rigidbody2D rigidBody;
+    protected Vector2 velocity;
+    protected ContactFilter2D contactFilter;
+    protected RaycastHit2D[] hitBuffer = new RaycastHit2D[16];
+    protected const float minMoveDistance = 0.001f;
+    protected const float shellRadius = 0.01f;
+
+    void OnEnable()
+    {
+        rigidBody = GetComponent<Rigidbody2D> ();
+    }
+
+    // Use this for initialization
+    void Start () {
+        contactFilter.useTriggers = false;
+        contactFilter.SetLayerMask(Physics2D.GetLayerCollisionMask(gameObject.layer));
+        contactFilter.useLayerMask = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void FixedUpdate() {
+        velocity += gravityModifier * Physics2D.gravity * Time.deltaTime;
+        Vector2 deltaPosition = velocity * Time.deltaTime;
+        Vector2 move = Vector2.up * deltaPosition.y;
+        Movement(move);
+    }
+
+    void Movement (Vector2 move) {
+        float distance = move.magnitude;
+
+        if (distance > minMoveDistance) {
+            int count = rigidBody.Cast(move, contactFilter, hitBuffer, distance + shellRadius);
+        }
+
+        rigidBody.position = rigidBody.position + move;
+    }
+}
