c6790e77135436cc87e8f9c67c1a4b162a15245e
Jacobo Arcila Duque <jacoboarcila@hotmail.com>
2018-05-23 22:37:17 -0500

    Flying Platform added

---
+++ KidBetweenDimensions/Assets/Flower.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Flower : MonoBehaviour {
+	Animator anim;
+
+
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.tag == "power") {
+			anim.SetTrigger ("Fly");
+
+			
+		}
+
+
+
+	}
+
+	void OnTriggerStay2D (Collider2D other)
+	{
+		if(other.tag=="player"){
+			transform.SetParent (transform);
+
+
+	}
+
+}
+
+}
--- KidBetweenDimensions/Assets/Patrol.cs
+++ KidBetweenDimensions/Assets/Patrol.cs
@@ -3,10 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Patrol : MonoBehaviour {
+	public float health;
 	public float speed;
 	private bool movingRight= true;
 	public Transform groundDetection;
+	public GameObject particleEffect;
+	public float camShakeAmt = 0.1f;
+	CameraShake camShake;
+	Animator anim;
+	float delay = 0.6f;
 	// Use this for initialization
+
+	void Start (){
+		anim = GetComponent<Animator> ();
+
+	}
 
 	
 	// Update is called once per frame
@@ -23,8 +34,37 @@ public class Patrol : MonoBehaviour {
 			} else {
 				transform.eulerAngles = new Vector3(0,0,0);
 				movingRight = true;
+
+
 	}
 	}
+
+
+
+
+
+
+
+			
+}
+
+	void OnTriggerEnter2D(Collider2D other){
+
+		if (other.tag == "power") {
+			health = (health - 10f);
+
+	}
+
+		if (health <= 0f) {
+			anim.SetTrigger ("Dead");
+
+			Destroy (gameObject, delay);
+		}
+
+
+
+
+
 }
 }
 
