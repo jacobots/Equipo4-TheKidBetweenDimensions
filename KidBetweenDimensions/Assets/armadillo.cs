ac3d3fb6337bdb37308cb9ad07201fc5a48e8c8e
Jacobo Arcila Duque <jacoboarcila@hotmail.com>
2018-05-26 01:45:50 -0500

    Armadillo

---
+++ KidBetweenDimensions/Assets/armadillo.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class armadillo : MonoBehaviour {
+	Animator anim;
+	public float health;
+	float delay = 0.6f;
+	public float camShakeAmt = 0.1f;
+	CameraShake camShake;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+
+		if (other.tag == "power") {
+			health = (health - 10f);
+			print ("ColisiónConPower");
+		}
+
+		if (health <= 0f) {
+			anim.SetTrigger ("Dead");
+			camShake.Shake (camShakeAmt, 0.1f);
+			Destroy (gameObject, delay);
+		}
+
+	}
+
+
+
+
+}
