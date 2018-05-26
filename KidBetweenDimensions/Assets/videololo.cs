179045ec969a5077f23250c328c288bc6ee59269
joseggonzalezl <josegonzalez9616@hotmail.com>
2018-05-26 10:59:20 -0500

    Finalisimo

---
+++ KidBetweenDimensions/Assets/videololo.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class videololo : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+
+			if(Input.GetButton("Jump")) {
+				SceneManager.LoadScene("Cinematica");
+			}
+
+		
+	}
+}
