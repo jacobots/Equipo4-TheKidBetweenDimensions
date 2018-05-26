93f5c4cac5a1fbdd7891ce66f733694b0ac9d306
Jacobo Arcila Duque <jacoboarcila@hotmail.com>
2018-05-26 05:49:17 -0500

    Telef

---
+++ KidBetweenDimensions/Assets/teleferico.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class teleferico : MonoBehaviour {
+	Animator anim;
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> ();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnTriggerEnter2D(Collider2D other){
+		if (other.tag == "Player") {
+			anim.SetTrigger ("moving");
+
+	}
+
+		if (other.gameObject.tag == "Player") {
+			other.transform.parent = transform;
+
+		}
+}
+}
