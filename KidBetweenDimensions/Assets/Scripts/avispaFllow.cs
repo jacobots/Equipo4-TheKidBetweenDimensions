85b9a660971c17a9d795e76251691a524e9380da
Andres <yugos802@hotmail.com>
2018-05-26 07:43:34 -0500

    avispa

---
+++ KidBetweenDimensions/Assets/Scripts/avispaFllow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class avispaFllow : MonoBehaviour {
+
+	public float speed;
+
+	private Transform target;
+	public GameObject player;
+
+	// Use this for initialization
+	void Start () {
+
+		target = player.GetComponent<Transform> (); 
+
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		transform.position = Vector2.MoveTowards (transform.position, target.position, speed );
+		
+	}
+}
