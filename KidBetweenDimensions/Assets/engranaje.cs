cbe3d570790a1d5f4d11526c7bf07ea5555c76b6
joseggonzalezl <josegonzalez9616@hotmail.com>
2018-05-26 01:40:29 -0500

    Bosque, teleferico, engranajes

---
+++ KidBetweenDimensions/Assets/engranaje.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class engranaje : MonoBehaviour {
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		this.transform.Rotate (0, 0, speed);
+		
+	}
+}
